81d1baaca4da4b29190b95a8ca0514a728505d89
manjunath <manju.ole23@GMAIL.COM>
2016-05-30 14:14:04 +0530

    Added New Pages and Modified Code.

--- src/test/java/com/hs/pages/Webform2Page.java
+++ src/test/java/com/hs/pages/Webform2Page.java
@@ -1,5 +1,7 @@
 package com.hs.pages;
 
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -19,32 +21,39 @@ String expectedResult=null;
 boolean flag;
 
 By name=By.id("ctl00_lblLoggedInUser");
-
 By DashboardMessage=By.id("ctl00_cphMain_Label2");
-
 By MyrequestStatusMessage=By.id("ctl00_cphMain_lbl_RecentHeading");
-
 By ReportiesRequestMessage=By.id("ctl00_cphMain_lbl_RecentHeadingOther");
-
 By celebrationsMessage=By.id("ctl00_cphMain_lbl_RecentHeadingOther");
-
 By EventsMessage=By.id("ctl00_cphMain_Label3");
-
-By AttendeneceRegulationCount=By.xpath("//tr[@id='ctl00_cphMain_MPRequest']/td[2]//following-sibling::td[1]");
-By onDutycount=By.xpath("//tr[@id='ctl00_cphMain_OTRequest']/td[2]/following-sibling:: td[1]");
+//By AttendeneceRegulationCount=By.xpath("//tr[@id='ctl00_cphMain_MPRequest']/td[2]//following-sibling::td[1]");
+//By onDutycount=By.xpath("//tr[@id='ctl00_cphMain_OTRequest']/td[2]/following-sibling:: td[1]");
 By LeaveRequestCount=By.xpath("//tr[@id='ctl00_cphMain_LeaveRequest']/td[2]/following-sibling:: td[1]");
-By CompoffCount=By.xpath("//tr[@id='ctl00_cphMain_CORequest']/td[2]/following-sibling:: td[1]");
+//By CompoffCount=By.xpath("//tr[@id='ctl00_cphMain_CORequest']/td[2]/following-sibling:: td[1]");
 By Logout= By.partialLinkText("Logout");
 
-By Birthdays= By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl6']]/td/a[@title='Click To View']");
-By birthdaylabel=By.id("ctl00_cphMain_lbl_lbl6");
 
+
+By MyrequestHead=By.id("ctl00_cphMain_lbl_RecentHeading");
+By AttendeceRegulation=By.id("ctl00_cphMain_lbl_lbl1");
+By AttendenceRegulationCount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl1']]/td/a[@title='Click To View']");
+By  onDuty=By.id("ctl00_cphMain_lbl_lbl2");
+By onDutyCount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl2']]/td/a[@title='Click To View']");
+By Leave=By.id("ctl00_cphMain_lbl_lbl3");
+By LeaveCount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl3']]/td/a[@title='Click To View']");
+By Compoff=By.id("ctl00_cphMain_lbl_lbl4");
+By CompoffCount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl4']]/td/a[@title='Click To View']");
+
+By celebrationsAndannouncements=By.id("ctl00_cphMain_Label1");
+By Birthdayscount= By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl6']]/td/a[@title='Click To View']");
+By birthdaylabel=By.id("ctl00_cphMain_lbl_lbl6");
 By NewJoinerslabel=By.id("ctl00_cphMain_lbl_lbl7");
 By NewJoinersCount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl7']]/td/a[@title='Click To View']");
-
-
 By ServiceAnnviresary=By.id("ctl00_cphMain_lbl_lbl9");
 By ServiceAnnviresarycount=By.xpath("//tr[td/span[@id='ctl00_cphMain_lbl_lbl9']]/td/a[@title='Click To View']");
+
+
+
 
 
 
@@ -98,8 +107,8 @@ public String getNumberofBirthdays() throws Exception
 {
 	try {
 		
-		actualResult=getText(Birthdays);
-		System.out.println(actualResult);
+		actualResult=getText(Birthdayscount);
+		System.out.println(actualResult + "  Birthdays Count");
 		return actualResult;
 		
 	} 
@@ -128,7 +137,7 @@ public String getNumberofNewJoiners() throws Exception
 	try {
 		
 		actualResult=getText(NewJoinersCount);
-		System.out.println(actualResult);
+		System.out.println(actualResult + "  New Joiners Count");
 		return actualResult;
 		
 	} 
@@ -160,7 +169,7 @@ public String getServiceAnniversary() throws Exception
 	try {
 		
 		actualResult=getText(ServiceAnnviresarycount);
-		System.out.println(actualResult);
+		System.out.println(actualResult + "   Service Annviresary Count");
 		return actualResult;
 		
 	} 
@@ -168,8 +177,138 @@ public String getServiceAnniversary() throws Exception
 	{
 		throw new Exception("Failed while getting the ServiceAnniversary count" + e.getLocalizedMessage());
 	}
+}
+
+public boolean verifyMyrequestElement() throws Exception
+{
+try 
+{
+	flag=isElementPresent(MyrequestHead);
+	Assert.assertTrue(flag,"MyRequest element is not  displaying on the page");
+	return flag;
+	
+} catch (NoSuchElementException e) 
+{
+	throw new Exception("Filed while verifying the My Request Element "+e.getLocalizedMessage());
+}	
+}
+
+public boolean verifyAttendenceregulationdisplayed() throws Exception
+{
+try {
+	
+	flag=isElementPresent(AttendeceRegulation);
+	Assert.assertTrue(flag,"Attendence Reulation Label is Missing");
+	return flag;
+	
+} catch (Exception e) {throw new Exception("Failed while  verifying the attendence Regulations Label" +e.getLocalizedMessage());
+}	
+}
+
+public void getAttendenceRegulationsCount() throws Exception
+{
+try {
+	actualResult=getText(AttendenceRegulationCount);
+	System.out.println(actualResult + "  Attendence Regulations Count");
+	
+} catch (Exception e) {
+	throw new Exception("Failed while Getting the Count of Attendence Regulations" + e.getMessage());
+}	
+}
+
+
+public boolean verifyOnDutydisplayed() throws Exception
+{
+try {
+	
+	flag=isElementPresent(onDuty);
+	Assert.assertTrue(flag,"OnDuty Label is Missing");
+	return flag;
+	
+} catch (Exception e) {throw new Exception("Failed while  verifying the OnDuty Label" +e.getLocalizedMessage());
+}	
+}
+
+public void getOndutyCount() throws Exception
+{
+try {
+	actualResult=getText(onDutyCount);
+	System.out.println(actualResult + "  On Duty Count");
+	
+} catch (Exception e) {
+	throw new Exception("Failed while Getting the Count of OndutyCount" + e.getMessage());
+}	
+}
+
+public boolean verifyleaveDisplayed() throws Exception
+{
+try 
+{
+	flag=isElementPresent(Leave);
+	Assert.assertTrue(flag,"Leave Element is not found on the Page");
+	return flag;
+} catch (Exception e) 
+{
+	throw new Exception("Failed while Verifying the Leave Label " + e.getLocalizedMessage());
+}	
+
+}
+
+public void getLeaveCount() throws Exception, NoSuchElementException
+{
+try {
+	
+	expectedResult=getText(LeaveCount);
+	System.out.println(expectedResult + "  Leave Count");
+	
+} catch (NoSuchElementException e) {
+	throw new Exception("Failed while getting the count of leave" + e.getMessage());
+}	
+
+}
+
+public boolean verifyCompoffDisplayed() throws Exception
+{
+try 
+{
+	flag=isElementPresent(Compoff);
+	Assert.assertTrue(flag,"Compoff  Element is not found on the Page");
+	return flag;
+} catch (Exception e) 
+{
+	throw new Exception("Failed while Verifying the Comp off Label " + e.getLocalizedMessage());
+}	
+
+}
+
+public void getCompOffCount() throws Exception, NoSuchElementException
+{
+try {
+	
+	expectedResult=getText(CompoffCount);
+	System.out.println(expectedResult+ " Leave Count");
+	
+} catch (NoSuchElementException e) {
+	throw new Exception("Failed while getting the count of Comp off " + e.getMessage());
+}	
+
+}
+
+public boolean verifyCelebrationsAndAnnouncementsDisplayed() throws Exception
+{
+try 
+{
+	flag=isElementPresent(celebrationsAndannouncements);
+	Assert.assertTrue(flag,"Celebrations   Element is not found on the Page");
+	return flag;
+} catch (Exception e) 
+{
+	throw new Exception("Failed while Verifying the Celebrations Label " + e.getLocalizedMessage());
+}	
+
+}
 
 
 }
-}
+
 
--- src/test/java/com/hs/tests/LoginTest.java
+++ src/test/java/com/hs/tests/LoginTest.java
@@ -24,7 +24,6 @@ public void clickonSubmitButton() throws Exception
 	try 
 	{
 		
-		
 		objloginpage= new LoginPage(uiDriver);
 		objloginpage.verifyUsername();
 		String userName=getExcelHRMSLogin(1, 1);
@@ -50,6 +49,65 @@ public void clickonSubmitButton() throws Exception
 		throw new Exception("FAILED VERIFYING the LoginPage   TESTCASE" + "\n clickonSubmitButton" +e.getLocalizedMessage());
 	}
 	}
+
+@Test(priority=2)
+public void MyRequestStatusDetails() throws Exception
+{
+	try {
+		objloginpage= new LoginPage(uiDriver);
+		objloginpage.verifyUsername();
+		String username=getExcelHRMSLogin(1, 1);
+		objloginpage.enterUsername(username);
+		objloginpage.verifyPassword();
+		String password=getExcelHRMSLogin(1, 2);
+		objloginpage.enterPassword(password);
+		objwebformpage=objloginpage.clickonSubmitButton();
+		objwebformpage.verifyMyrequestElement();
+		objwebformpage.verifyAttendenceregulationdisplayed();
+		objwebformpage.getAttendenceRegulationsCount();
+		objwebformpage.verifyOnDutydisplayed();
+		objwebformpage.getOndutyCount();
+		objwebformpage.verifyleaveDisplayed();
+		objwebformpage.getLeaveCount();
+		objwebformpage.verifyCompoffDisplayed();
+		objwebformpage.getCompOffCount();
+		Thread.sleep(4000);
+		objwebformpage.clickOnLogoutButton();
+		
+	} catch (Exception e)
+	{
+		throw new Exception("Failed Test case of My request Status " + e.getMessage());
+	}
+	}
+@Test(priority=3)
+public void getCelebrationsdetails() throws Exception
+{
+try
+{
+	objloginpage= new LoginPage(uiDriver);
+	objloginpage.verifyUsername();
+	String username=getExcelHRMSLogin(1, 1);
+	objloginpage.enterUsername(username);
+	objloginpage.verifyPassword();
+	String password=getExcelHRMSLogin(1, 2);
+	objloginpage.enterPassword(password);
+	objwebformpage=objloginpage.clickonSubmitButton();
+	objwebformpage.verifyCelebrationsAndAnnouncementsDisplayed();
+	objwebformpage.isbirthdaysTextdisplayed();
+	objwebformpage.getNumberofBirthdays();
+	objwebformpage.isNewJoinersTextdisplayed();
+	objwebformpage.getNumberofNewJoiners();
+	objwebformpage.isServiceAnniversaryTextdisplayed();
+	objwebformpage.getServiceAnniversary();
+	Thread.sleep(4000);
+	objwebformpage.clickOnLogoutButton();
+	
+} 
+catch (Exception e) 
+{
+	throw new Exception("Failed the test case of Celebrations and announcements" + e.getMessage());
+}	
+}
 @Test
 public String getExcelHRMSLogin(int row, int column) throws Exception
 {
